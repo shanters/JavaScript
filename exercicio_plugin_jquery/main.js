bdc9623a03f4bc4d663d75ad36d9b96dae28feec
shanters <128517750+shanters@users.noreply.github.com>
2023-04-12 17:04:34 -0300

    Exercício plugin jquery

---
+++ exercicio_plugin_jquery/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+    $('#celular').mask('(00) 00000-0000')
+    $('#cpf').mask('000.000.000-00')
+    $('#cep').mask('00000-000')
+
+    $('form').validate({
+    submitHandler: function(form) {
+            alert('Cadastrado!')
+        }
+
+    })
+})
